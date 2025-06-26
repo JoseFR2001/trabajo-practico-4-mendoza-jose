5880f103bfb79bb17075c769e945ed37d0451cfa
JoseFR2001 <jose.rafael.mendoza.2001@gmail.com>
2025-06-26 11:56:17 -0300

    Terminando el app.js

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 import express from "express"
 import dotenv from "dotenv"
+import { initDB } from "./src/config/database";
+import routeCharacter from "./src/routes/charater.routes";
+
 
 dotenv.config()
 
 const app = express();
 app.use(express.json())
 
+app.use("/", routeCharacter)
 const PORT = process.env.PORT;
+
+initDB().then(() =>  {
+    app.listen(PORT, () =>{
+        console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    })
+    
+})
